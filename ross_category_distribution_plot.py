1dc2d30cab07ac128a2e3a59c1107ad389756b69
Yutong FEI <yutong.fei@eleves.ec-nantes.fr>
2025-07-13 23:19:57 +0200

    Update ross_category_distribution_plot.py

--- ross_category_distribution_plot.py
+++ ross_category_distribution_plot.py
@@ -7,8 +7,8 @@ path = "XXX.csv"
 df = pd.read_csv(path)
 df.head()
 
-df_count = df.groupby(['argument categories', 'open science taxonomy']).size().reset_index(name='count')
-df_pivot = df_count.pivot(index='argument categories', columns='open science taxonomy', values='count').fillna(0)
+df_count = df.groupby(['the first column', 'the second column']).size().reset_index(name='count')
+df_pivot = df_count.pivot(index='the first column', columns='the second column', values='count').fillna(0)
 df_percent = df_pivot.div(df_pivot.sum(axis=1), axis=0) * 100
 
 
@@ -42,8 +42,8 @@ for i, (idx, row) in enumerate(df_pivot.iterrows()):
     total_count = int(row.sum())
     ax.text(i, cumulative + 0.05, f"{total_count}", ha='center', va='bottom', fontsize=10, color='black', fontweight='bold')
 
-ax.set_title("Distribution of Open Science Taxonomy across Identified Categories of Critical Arguments", fontsize=16)
-ax.set_xlabel("Argument Categories", fontsize=14)
+ax.set_title("Distribution of the second column across Identified Categories of the first column", fontsize=16)
+ax.set_xlabel("Categories of the first column", fontsize=14)
 ax.set_ylabel("Count", fontsize=14)
 plt.xticks(rotation=45, ha="right", fontsize=10)
 plt.yticks(fontsize=10)
